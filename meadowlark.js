e70f5c02b32c32e8601d5b05483cb9091cfa103d
williamowright <william.o.wright1997@gmail.com>
2022-01-09 14:16:29 -0600

    Moved 'fortune cookie' into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+const fortune = require("./lib/fortune");
 
 const app = express();
 
@@ -18,17 +19,8 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.render("home"));
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
-
 app.get("/about", (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render("about", { fortune: randomFortune });
+  res.render("about", { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
